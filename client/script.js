c042dd4b70cbdcad539427e969838ca51b500d90
Chen Roth <rothchen@gmail.com>
2021-08-18 16:51:49 +0300

    add page.js and redirect all unmatched traffic in server to index page

--- client/script.js
+++ client/script.js
@@ -1,12 +1,5 @@
 main();
 
+// https://github.com/visionmedia/page.js/
 async function main() {
-  console.log(
-    "%cyou can edit your code in %cscript.js!",
-    "color: lightblue; font-size: 18px; font-family: roboto",
-    "color: red; font-size: 18px"
-  );
-
-  const welcomeMessage = await $.get("/api/");
-  alert(welcomeMessage);
-}
+  }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 
 // we run this require to make the sql connection code execute early
 // this way, by the time the HTTP server is up, we already have an SQL connection
@@ -19,6 +20,8 @@ app.use("/api", api);
 
 // server client static files (html, client js, css, images etc.)
 app.use("/", express.static("client"));
+
+app.use('*', (req, res) => res.sendFile(path.join(__dirname, 'client', 'index.html')));
 
 app.listen(PORT, () => {
   console.log(`Server is up at ${PORT}`);
